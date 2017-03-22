399e8ecd45359012ac0c691ee3c30a973f5122bb
Dan Weatherill <daniel.weatherill@physics.ox.ac.uk>
2017-03-22 17:20:43 +0000

    start on dashboard2

--- cmake/host_specific.cmake
+++ cmake/host_specific.cmake
@@ -5,11 +5,28 @@ if( ${host} STREQUAL "pplxdt43")
   set(RTTR_DIR "/home/weatherill/Software/rttr-0.9.5-src/build/install")
   list(APPEND CMAKE_PREFIX_PATH ${RTTR_DIR})
   list(APPEND CMAKE_PREFIX_PATH "/local/home/weatherill/")
+#   list(APPEND CMAKE_PREFIX_PATH "/local/home/weatherill/anaconda/")
   set(CMAKE_LIBRARY_ARCHITECTURE x86_64-linux-gnu)
-  set(ENV{LD_LIBRARY_PATH} "/local/home/weatherill/lib:/local/home/weatherill/anaconda/lib")
+  set(Qt5_DIR "/usr/lib/")
   
-
-
+#   set(ENV{LD_LIBRARY_PATH} "/local/home/weatherill/lib:/local/home/weatherill/anaconda/lib")
+#   set(ENV{LD_RUN_PATH} "/local/home/weatherill/anaconda/lib")
+  
+#   find_library(png16 png16)
+#   message(STATUS "libpng: ${png16}")
+#   
+#   find_library(icui18n icui18n)
+#   message(STATUS "libicui18n: ${icui18n}")
+#   
+#   find_library(icuuc icuuc)
+#   message(STATUS "icuuc: ${icuuc}")
+#   
+#   find_library(icudata icudata)
+#   message(STATUS "icudata: ${icudata}")
+# 
+#   set(HOST_SPEC_LIBRARIES "${png16};${icui18n};${icuuc};${icudata}")
+#   
+  
 elseif( ${host} STREQUAL "linux-desktop.lan")
     set(RTTR_DIR "/home/danw/Software/rttr-0.9.5-src/build/install")
     list(APPEND CMAKE_PREFIX_PATH ${RTTR_DIR})
