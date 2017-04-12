9d0024db0ffc779972164e97f434fbad2f612ec5
Dan Weatherill <daniel.weatherill@physics.ox.ac.uk>
2017-04-12 12:28:40 +0100

    hvbias header

---
+++ devices/archon/archon_module_hvxbias.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "archon_modules.h"
+#include "archon_module_generic_bias.h"
+#include <rttr/registration>
+
+
+namespace foxtrot
+{
+  namespace devices
+  {
+    class archon;
+    class ArchonHVX : public ArchonModule 
+    {
+      RTTR_ENABLE(ArchonModule)
+      
+    public:
+      static std::unique_ptr<ArchonModule> constructModule(archon& arch, int modpos);
+      
+    virtual const string getTypeName() const override;
+    virtual void update_variables() override;
+    
+    	  
+    
+    
+    
+      
+    };
+  
+  
