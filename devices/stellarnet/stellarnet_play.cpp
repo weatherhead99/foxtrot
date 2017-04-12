63b2c6bc4cdcbba12752ce8336a65e4785dd2238
Dan Weatherill (OPMD lab) <daniel.weatherill@physics.ox.ac.uk>
2017-04-12 18:20:22 +0100

    forgot stellarnet test program

---
+++ devices/stellarnet/stellarnet_play.cpp
@@ -0,0 +1,26 @@
+#include <backward.hpp>
+#include "stellarnet.h"
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(int argc, char** argv)
+{
+  backward::SignalHandling sh;
+  foxtrot::devices::stellarnet spectro("/home/dweatherill/Software/stellarnet/files_to_copy/stellarnet.hex",1000);
+ 
+  cout << "reading spectrum..." << endl;
+  auto dat = spectro.read_spectrum(8000);
+  
+  std::ofstream ofs("spectrum.txt");
+  
+  for(auto& d : dat)
+  {
+    
+    ofs << d << " ";
+  }
+  
+  
+
+}
