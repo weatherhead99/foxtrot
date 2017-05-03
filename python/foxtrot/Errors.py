131580d5d1efaf41afb1cfcd9b4e70382bfcba41
Dan Weatherill <danw@linux-desktop.weatherill.dyndns.org>
2017-05-03 22:35:32 +0100

    add errors to python

---
+++ python/foxtrot/Errors.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  3 22:34:46 2017
+
+@author: danw
+"""
+
+class DeviceError(Exception):
+    pass
+
+class ProtocolError(Exception):
+    pass
