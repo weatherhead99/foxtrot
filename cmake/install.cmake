db179333de925491ff546045df614524e83cdb94
Dan Weatherill <daniel.weatherill@physics.ox.ac.uk>
2017-05-23 15:41:20 +0100

    move more install stuff into cmake file

--- cmake/install.cmake
+++ cmake/install.cmake
@@ -1,11 +1,36 @@
 include(GNUInstallDirs)
 
-install(TARGETS foxtrot EXPORT ft_targets LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}/foxtrot/ )
+#foxtrot
+install(TARGETS foxtrot EXPORT ft_targets LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}/foxtrot/ 
+	  ARCHIVE DESTINATION ${CMAKE_INSTALL_LIBDIR}/foxtrot/)
+	  
 file(GLOB ft_includes ${CMAKE_SOURCE_DIR}/include/*.h)
 message(STATUS "ft includes: ${ft_includes}")
 install(FILES ${ft_includes} DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}/foxtrot/ COMPONENT devel)
 
+#server
+install(FILES ${CMAKE_SOURCE_DIR}/server/foxtrot.proto DESTINATION ${CMAKE_INSTALL_DATADIR}/foxtrot/)
 
+file(GLOB ft_server_includes ${CMAKE_SOURCE_DIR}/server/*.h)
+install(FILES ${ft_server_includes} ${PROTO_HDRS} ${PROTO_GRPC_HDRS} 
+	DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}/foxtrot/ COMPONENT devel)
+
+	
+install(TARGETS foxtrotserv LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}/foxtrot/ 
+	ARCHIVE DESTINATION ${CMAKE_INSTALL_LIBDIR}/foxtrot/)
+
+install(TARGETS exptserve RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR})
+
+
+#client
+
+file(GLOB ft_client_includes ${CMAKE_SOURCE_DIR}/client/*.h)
+install(FILES ${ft_client_includes} DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}/foxtrot)
+
+
+install(TARGETS foxtrotclient LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}/foxtrot 
+	  ARCHIVE DESTINATION ${CMAKE_INSTALL_LIBDIR}/foxtrot)
+	
 #cmake package files
 include(CMakePackageConfigHelpers)
 configure_package_config_file(${CMAKE_SOURCE_DIR}/cmake/foxtrotConfig.cmake.in foxtrotConfig.cmake 
