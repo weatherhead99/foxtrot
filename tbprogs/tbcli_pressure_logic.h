fc48978e0e68a0625dd504a9d36450f61da485e0
Dan Weatherill <daniel.weatherill@physics.ox.ac.uk>
2017-05-23 14:01:57 +0100

    removed OPMD specific stuff in preparation to separate foxtrot core from the rest

--- tbprogs/tbcli_pressure_logic.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "client.h"
-
-int find_pressure_gauge(const foxtrot::servdescribe& cl);
-
-double get_pump_pressure(foxtrot::Client& cl, int devid);
-double get_cryostat_pressure(foxtrot::Client& cl, int devid);
