39ec6dc52515456d8a763e4531f701877fde5317
Dan Weatherill <daniel.weatherill@physics.ox.ac.uk>
2018-04-30 01:14:40 +0100

    convenience functions for watchdog

--- watchdog/WatchDogHeater.cpp
+++ watchdog/WatchDogHeater.cpp
@@ -1,3 +1,75 @@
 #include "WatchDogHeater.h"
+#include <algorithm>
+
+
+foxtrot::WatchDogHeater::WatchDogHeater(const std::string& name, int tick_check, int history_limit)
+: WatchDogEvent(name, tick_check), history_limit_(history_limit)
+{
+}
+
+void foxtrot::WatchDogHeater::setup_devices(const foxtrot::servdescribe& desc)
+{
+    devid_heater_ = foxtrot::find_devid_on_server(desc,"ArchonHeaterX");
+    if(devid_heater_ < 0)
+    {
+        throw std::runtime_error("couldn't find heater on server!");
+    }
+    
+}
+
+double foxtrot::WatchDogHeater::get_heater_target(foxtrot::Client& cl)
+{
+    std::vector<foxtrot::ft_variant> args{0};
+    auto resp = cl.InvokeCapability(devid_heater_,"getHeaterTarget",args.begin(), args.end());
+    return boost::get<double>(resp);
+}
+
+double foxtrot::WatchDogHeater::get_stage_temp(foxtrot::Client& cl)
+{
+    auto resp = cl.InvokeCapability(devid_heater_,"getTempA");
+    store_value(stage_history, boost::get<double>(resp));
+    return boost::get<double>(resp);
+}
+
+double foxtrot::WatchDogHeater::get_tank_temp(foxtrot::Client& cl)
+{
+    auto resp = cl.InvokeCapability(devid_heater_,"getTempB");
+    store_value(tank_history, boost::get<double>(resp));
+    return boost::get<double>(resp);
+}
+
+
+void foxtrot::WatchDogHeater::store_value(std::deque<double>& history, double val)
+{
+    while(history.size() >= history_limit_)
+    {
+        history.pop_front();
+    }
+    
+    history.push_back(val);
+}
+
+
+double foxtrot::WatchDogHeater::partial_average(const std::deque<double>& history, int average_size)
+{
+    auto sum = std::accumulate(history.end() - average_size, history.end(), 0);
+    return sum / average_size;
+    
+}
+
+double foxtrot::WatchDogHeater::rate_of_change(const std::deque<double>& history, int size)
+{
+    auto diff = std::accumulate(history.rend(), history.rend() + size, 0);
+    return diff / size;
+    
+}
+
+bool foxtrot::WatchDogHeater::rising(const std::deque<double>& history, int size)
+{
+  auto rate = rate_of_change(history,size);
+  return rate > 0;
+    
+};
+
 
 
--- watchdog/WatchDogHeater.h
+++ watchdog/WatchDogHeater.h
@@ -1,18 +1,37 @@
 #pragma once
 #include "WatchDogEvent.h"
+#include <client.h>
+#include <deque>
 
 namespace foxtrot {
     
     class WatchDogHeater : public WatchDogEvent
     {
     public:
-        WatchDogHeater(const std::string& name, int tick_check);
-        void setup_devices(const int & desc) override;
+        WatchDogHeater(const std::string& name, int tick_check, int history_limit);
+        void setup_devices(const foxtrot::servdescribe& desc) override;
+    
+    protected:
+        double get_stage_temp(foxtrot::Client& cl);
+        double get_tank_temp(foxtrot::Client& cl);
+        double get_heater_target(foxtrot::Client& cl);
+        
+        double partial_average(const std::deque<double>& history, int average_size);
+        double rate_of_change(const std::deque<double>& history, int size);
+        
+        bool rising(const std::deque<double>& history, int size);
         
     private:
+        void store_value(std::deque<double>& history, double val);
+        int history_limit_;
         int devid_heater_;
+        std::deque<double> stage_history;
+        std::deque<double> tank_history;
         
     };
+    
+    
+    
     
     
 }
