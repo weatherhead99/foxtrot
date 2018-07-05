27c303fb315a514915faae98542a999e3ffddbd4
Dan Weatherill <daniel.weatherill@physics.ox.ac.uk>
2018-07-05 03:00:55 +0100

    support new QwT version

--- src/SerialProtocol.cpp
+++ src/SerialProtocol.cpp
@@ -10,6 +10,7 @@ foxtrot::SerialProtocol::SerialProtocol(const foxtrot::parameterset*const instan
 std::string foxtrot::SerialProtocol::read_until_endl(char endlchar)
 {
   std::cout << "called unimplemented function read_until_endl" << std::endl;
+  return std::string("");
 
 }
 
