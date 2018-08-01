bb074fb3287cdb0a2612611bc1d94d870e277be9
Dan Weatherill <daniel.weatherill@physics.ox.ac.uk>
2018-08-01 18:18:50 +0100

    java now building

--- java/src/FoxtrotClient.java
+++ java/src/FoxtrotClient.java
@@ -2,11 +2,14 @@ package io.foxtrot;
 
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import io.foxtrot.exptserveGrpc.exptserveStub;
+import io.foxtrot.exptserveGrpc.exptserveBlockingStub;
 
 
 public class FoxtrotClient {
     private final ManagedChannel channel;
-    
+    private final exptserveStub asyncStub;
+    private final exptserveBlockingStub blockingStub;
     
     public FoxtrotClient(String host, int port) {
         this(ManagedChannelBuilder.forAddress(host,port).usePlaintext().build());
@@ -14,6 +17,8 @@ public class FoxtrotClient {
     
     FoxtrotClient(ManagedChannel channel) {
         this.channel = channel;
+        blockingStub = exptserveGrpc.newBlockingStub(channel);
+        asyncStub = exptserveGrpc.newStub(channel);
     }
     
 }
