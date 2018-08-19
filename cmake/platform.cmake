ad0e046c088c7e1bd678c06df24e49fa14085bb3
Dan Weatherill (Windows 10 VM) <dan.weatherill@cantab.net>
2018-08-19 02:14:38 +0100

    Fix a few more windows build issues
    DLLs now export all symbols by default

--- cmake/platform.cmake
+++ cmake/platform.cmake
@@ -1,5 +1,11 @@
 
 #enable c++ standard exception handling on windows
+#set dynamic runtime
 if(MSVC)
 set(CMAKE_CXX_FLAGS "/EHsc")
+set(CMAKE_CXX_FLAGS_RELEASE "/MD")
+set(CMAKE_CXX_FLAGS_DEBUG "/MDd")
+
+set(CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS "ON")
+
 endif()
