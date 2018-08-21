adad50b358d582bbaf44d533e19f9dcebdaaf397
Daniel Weatherill <weatherill@physics.ox.ac.uk>
2018-08-21 15:11:05 +0100

    start on script to automate windows build on physics desktops

--- cmake/platform.cmake
+++ cmake/platform.cmake
@@ -2,10 +2,16 @@
 #enable c++ standard exception handling on windows
 #set dynamic runtime
 if(MSVC)
-set(CMAKE_CXX_FLAGS "/EHsc")
-set(CMAKE_CXX_FLAGS_RELEASE "/MD")
-set(CMAKE_CXX_FLAGS_DEBUG "/MDd")
+	set(CMAKE_CXX_FLAGS "/EHsc")
+	set(CMAKE_CXX_FLAGS_RELEASE "/MD")
+	set(CMAKE_CXX_FLAGS_DEBUG "/MDd")
 
-set(CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS "ON")
+	set(CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS "ON")
+
+	if(CMAKE_SIZEOF_VOID_P EQUAL 8)
+#64 bit build
+		set(PROJECTNAME_ARCHITECTURE "x64")
+
+	endif()
 
 endif()
---
+++ platformscripts/Physics_Windows_Desktop_Setup.bat
@@ -0,0 +1,20 @@
+ECHO "setting path to include local python scripts..."
+set PATH=%PATH%;%APPDATA%\Python\Python36\scripts
+set VCVARS_BAT=C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvars64.bat
+ECHO "installing conan..."
+pip install --user conan > NUL
+ECHO "adding conan remotes..."
+conan remote add inexorgame https://api.bintray.com/conan/inexorgame/inexor-conan 
+conan remote add bincrafters https://api.bintray.com/conan/bincrafters/public-conan
+conan remote add weatherhead99 https://api.bintray.com/conan/weatherhead99/conan_packages 
+ECHO "setting up build environment
+%VCVARS_BAT%
+ECHO "installing conan based dependencies for foxtrot..."
+cd %FT_DIR%
+mkdir ..\conanbuild
+cd ..\conanbuild
+conan install .. 
+ECHO "running cmake..."
+cmake .. -A x64 -DBUILD_DASHBOARD=OFF 
+ECHO "building foxtrot in RELEASE mode..."
+cmake --build . --config Release
