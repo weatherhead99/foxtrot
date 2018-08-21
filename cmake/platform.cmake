cfbd8a600fc0819582767ef55b8811b0c12e9585
Daniel Weatherill <weatherill@physics.ox.ac.uk>
2018-08-21 16:32:18 +0100

    work on export headers

--- cmake/platform.cmake
+++ cmake/platform.cmake
@@ -6,12 +6,16 @@ if(MSVC)
 	set(CMAKE_CXX_FLAGS_RELEASE "/MD")
 	set(CMAKE_CXX_FLAGS_DEBUG "/MDd")
 
-	set(CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS "ON")
+	#set(CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS "ON")
 
 	if(CMAKE_SIZEOF_VOID_P EQUAL 8)
-#64 bit build
+	#64 bit build
 		set(PROJECTNAME_ARCHITECTURE "x64")
 
 	endif()
 
+	#sets which winapi versions are needed, including e.g. killing
+	#a boost_log_abi error
+	add_definitions(-D_WIN32_WINNT=0x601)
+	
 endif()
