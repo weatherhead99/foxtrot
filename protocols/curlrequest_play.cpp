f24f3a1de19e9a18cf0264d8a826021f9f3559f9
Dan Weatherill <daniel.weatherill@physics.ox.ac.uk>
2019-02-15 13:44:32 +0000

    simple curl test program

---
+++ protocols/curlrequest_play.cpp
@@ -0,0 +1,18 @@
+#include "curlRequest.h"
+
+#include <iostream>
+
+using namespace foxtrot::protocols;
+
+int main(int, char**)
+{
+    CurlRequest proto;
+    
+    std::cout << "starting request" << std::endl;
+    auto out = proto.blocking_get_request("http://192.168.1.2/state.xml");
+    
+    std::cout << "request finished..." << std::endl;
+    
+    std::cout << "received data: " << out << std::endl;
+    
+}
