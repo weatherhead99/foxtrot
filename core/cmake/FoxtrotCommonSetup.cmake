9f603891632cf893a8fbe55c880395874c4f61e7
Dan Weatherill <daniel.weatherill@physics.ox.ac.uk>
2019-04-04 20:32:07 +0100

    factor out some common cmake code

---
+++ core/cmake/FoxtrotCommonSetup.cmake
@@ -0,0 +1,12 @@
+#setup conan build if necessary
+if(EXISTS ${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
+    message(WARNING "conanbuildinfo detected, building using
+    conan dependencies")
+    include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
+    set(FOXTROT_CORE_CONAN_BUILD TRUE)
+    conan_basic_setup()
+endif()
+
+
+#set shared library build by default
+set(BUILD_SHARED_LIBS ON CACHE BOOL "build shared libraries")
