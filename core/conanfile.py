83808bd879a870d184464a5b02e380302383268f
Dan Weatherill <daniel.weatherill@physics.ox.ac.uk>
2019-04-04 16:48:24 +0100

    git version information in conan

--- core/conanfile.py
+++ core/conanfile.py
@@ -4,11 +4,20 @@ from conans import ConanFile, CMake, tools
 bcs = "@bincrafters/stable"
 bbcs = "/1.69.0%s" % bcs
 
+def get_version():
+    git = tools.Git()
+    try:
+        return "%s_%s" % (git.get_branch(), git.get_revision())
+    except:
+        return None
+
 class FoxtrotCoreConan(ConanFile):
     name = "foxtrot_core"
     description = "core libraries for foxtrot"
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake", "virtualrunenv"
+    exports_sources = "*"
+    version = get_version()
     requires = "boost_log%s" % bbcs, \
         "grpc/1.17.2@inexorgame/stable",\
         "protobuf/3.6.1%s" % bcs, \
@@ -21,3 +30,5 @@ class FoxtrotCoreConan(ConanFile):
         env_build = tools.RunEnvironment(self)
         with tools.environment_append(env_build.vars):
             cmake.build()
+        
+        cmake.install()
