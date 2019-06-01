3a19d6dc32e37a8579af8969e9852955d5963810
Dan Weatherill <daniel.weatherill@physics.ox.ac.uk>
2019-06-01 01:00:41 +0100

    start on lsst package

---
+++ lsst/conanfile.py
@@ -0,0 +1,18 @@
+from conans import python_requires
+
+ftbase = python_requires("FoxtrotBuildUtils/0.1@weatherhead99/testing")
+
+class FoxtrotLSST(ftbase.FoxtrotCppPackage,
+                  metaclass = ftbase.FoxtrotCppMeta):
+    name="foxtrot_lsst"
+    description="LSST test stand specific stuff for foxtrot"
+    exports_sources = ("CMakeLists.txt", "*/CMakeLists.txt", 
+                       "testbench_setup_funcs.*", "tbprogs/*.h",
+                       "tbprogs/*.cpp", "autofilld/*.cpp",
+                       "autofilld/*.hh", "testbench_archon_heater.*")
+    requires = "OpenSSL/1.0.2r@conan/stable"
+
+    def requirements(self):
+        self.requires("foxtrot_client/%s@%s/%s" % (self.version,self.user,self.channel))
+        self.requires("foxtrot_protocols/%s@%s/%s" % (self.version,self.user,self.channel))
+        self.requires("foxtrot_devices/%s@%s/%s" % (self.version,self.user,self.channel))
