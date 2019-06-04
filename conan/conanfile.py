8e836563a5c012f2d4c18f2eb86c53d897854ce5
Dan Weatherill <daniel.weatherill@physics.ox.ac.uk>
2019-06-04 15:53:04 +0100

    HACK: use hardcoded version in conan packages, imrpove SOON

--- conan/conanfile.py
+++ conan/conanfile.py
@@ -2,16 +2,19 @@ from conans import tools, ConanFile, CMake
 
 hardcode_version="0.0.1dev"
 
+# def get_version():
+#     git = tools.Git()
+#     try:
+#         tag = git.get_tag()
+#         if git.get_tag() is not None:
+#             return tag
+#         else:
+#             return "%s_%s" % (git.get_branch(), git.get_revision()[:8])
+#     except:
+#         return hardcode_version
+
 def get_version():
-    git = tools.Git()
-    try:
-        tag = git.get_tag()
-        if git.get_tag() is not None:
-            return tag
-        else:
-            return "%s_%s" % (git.get_branch(), git.get_revision()[:8])
-    except:
-        return hardcode_version
+    return hardcode_version
 
 bcs = "@bincrafters/stable"
 bbcs = "/1.69.0%s" % bcs
