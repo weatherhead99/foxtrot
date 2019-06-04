0b7cb4733e20c418ae3ac326a0d4f0ddff31a011
Dan Weatherill <daniel.weatherill@physics.ox.ac.uk>
2019-06-05 00:07:10 +0100

    custom find libusb module

---
+++ core/cmake/Findlibusb.cmake
@@ -0,0 +1,11 @@
+
+find_path(LIBUSB_INCLUDE_DIR NAMES libusb.h PATH_SUFFIXES libusb-1.0)
+find_library(LIBUSB_LIBRARY NAMES usb-1.0 usb)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(libusb DEFAULT_MSG LIBUSB_INCLUDE_DIR LIBUSB_LIBRARY)
+
+add_library(libusb::libusb UNKNOWN IMPORTED)
+set_target_properties(libusb::libusb PROPERTIES IMPORTED_LOCATION ${LIBUSB_LIBRARY})
+set_target_properties(libusb::libusb PROPERTIES 
+                        INTERFACE_INCLUDE_DIRECTORIES ${LIBUSB_INCLUDE_DIR})
