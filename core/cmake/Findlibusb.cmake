339565024b89ee56ebdd825f0dbbc800e1db3f80
Dan Weatherill <daniel.weatherill@physics.ox.ac.uk>
2019-06-05 00:35:09 +0100

    port libusb finding to custom find module

--- core/cmake/Findlibusb.cmake
+++ core/cmake/Findlibusb.cmake
@@ -5,7 +5,11 @@ find_library(LIBUSB_LIBRARY NAMES usb-1.0 usb)
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(libusb DEFAULT_MSG LIBUSB_INCLUDE_DIR LIBUSB_LIBRARY)
 
-add_library(libusb::libusb UNKNOWN IMPORTED)
-set_target_properties(libusb::libusb PROPERTIES IMPORTED_LOCATION ${LIBUSB_LIBRARY})
-set_target_properties(libusb::libusb PROPERTIES 
-                        INTERFACE_INCLUDE_DIRECTORIES ${LIBUSB_INCLUDE_DIR})
+if(TARGET libusb::libusb)
+    message(STATUS "libusb target already exists, not doing anything")
+else()
+    add_library(libusb::libusb UNKNOWN IMPORTED GLOBAL)
+    set_target_properties(libusb::libusb PROPERTIES IMPORTED_LOCATION ${LIBUSB_LIBRARY})
+    set_target_properties(libusb::libusb PROPERTIES 
+                            INTERFACE_INCLUDE_DIRECTORIES ${LIBUSB_INCLUDE_DIR})
+endif()
