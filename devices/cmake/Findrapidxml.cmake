95e528e07b39cce685f6ac51ad704e9884a4eceb
Dan Weatherill <daniel.weatherill@physics.ox.ac.uk>
2019-06-04 20:00:32 +0100

    work on getting dependencies right for device

---
+++ devices/cmake/Findrapidxml.cmake
@@ -0,0 +1,8 @@
+find_path(RAPIDXML_INCLUDE "rapidxml.hpp" PATH_SUFFIXES rapidxml)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(rapidxml DEFAULT_MSG RAPIDXML_INCLUDE)
+
+add_library(rapidxml::rapidxml UNKNWON IMPORTED)
+set_target_properties(rapidxml::rapidxml PROPERTIES INTERFACE_INCLUDE_DIRECTORIES ${RAPIDXML_INCLUDE})
+
