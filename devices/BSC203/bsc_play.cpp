00fd85cf38085aa5ae371bf7e01d52423c33e4ec
Sergio Garcia <sergio@pplxdaq04.nat.physics.ox.ac.uk>
2019-07-09 17:42:30 +0100

    const reference argumentes added to some functions.

--- devices/BSC203/bsc_play.cpp
+++ devices/BSC203/bsc_play.cpp
@@ -34,16 +34,16 @@ int main(int argc,char** argv)
   jogstruct.subMsgID = 0x0009;
   jogstruct.chanIndent = 0x01;
   jogstruct.jogMode = 0x0002; //step
-  jogstruct.jogStepSize = 0x000000AA;
+  jogstruct.jogStepSize = 0x0000000A;
   jogstruct.jogStepRate = 0x000001F4;
   jogstruct.jogStepAccn = 0x000186A0;
   
-  motors.set_jog_parameters(foxtrot::devices::destination::sourceTIM101,&jogstruct);
+  motors.set_jog_parameters(foxtrot::devices::destination::sourceTIM101,jogstruct);
   
   foxtrot::devices::jogparams jogstruct_req = motors.request_jog_parameters(foxtrot::devices::destination::sourceTIM101);
   
   cout << "jog Mode requested: " << std::hex << jogstruct_req.jogMode << endl;
-  cout << "jog Mode Size requested: " << std::hex << jogstruct_req.jogStepSize << endl;
+  cout << "jog Step Size requested: " << std::hex << jogstruct_req.jogStepSize << endl;
   cout << "jog Step Accn requested: " << std::hex << jogstruct_req.jogStepAccn << endl;*/
   
   //motors.jog_move(foxtrot::devices::destination::sourceTIM101,foxtrot::devices::motor_channel_idents::channel_1, foxtrot::devices::jogdir::forward);
@@ -51,14 +51,14 @@ int main(int argc,char** argv)
   // SET POSITION COUNTER
   foxtrot::devices::pos_counter_params posparams;
   posparams.subMsgID = 0x0005;
-  posparams.chanIndent = 0x01;
+  posparams.chanIndent = 0x08;
   posparams.position = 0x0;
   posparams.encCount = 0x0;
   
-  motors.set_position_counter(foxtrot::devices::destination::sourceTIM101, &posparams);
+  motors.set_position_counter(foxtrot::devices::destination::sourceTIM101, posparams);
   
   motors.jog_move(foxtrot::devices::destination::sourceTIM101,foxtrot::devices::motor_channel_idents::channel_1, foxtrot::devices::jogdir::forward);
-  
+
   motors.jog_move(foxtrot::devices::destination::sourceTIM101,foxtrot::devices::motor_channel_idents::channel_4, foxtrot::devices::jogdir::forward);
   
   //foxtrot::devices::pos_counter_params posparams_rec = motors.request_position_counter(foxtrot::devices::destination::sourceTIM101);
@@ -71,26 +71,22 @@ int main(int argc,char** argv)
   absparams.chanIndent = 0x01;
   absparams.maxVoltage = 0x006E;
   absparams.stepRate = 0x00000014;
-  absparams.stepAccn = 0x000000F0;
+  absparams.stepAccn = 0x0000000F;
   
-  motors.set_move_absolute_parameters(foxtrot::devices::destination::sourceTIM101, &absparams);
+  motors.set_move_absolute_parameters(foxtrot::devices::destination::sourceTIM101, absparams);
   
   foxtrot::devices::move_absolute_params absparams_rec = motors.request_move_absolute_parameters(foxtrot::devices::destination::sourceTIM101);
   
   cout << "move_absolute channel : " << std::hex << absparams_rec.chanIndent << endl;
   cout << "move_absolute max voltage : " << std::hex << absparams_rec.maxVoltage << endl;
   cout << "move_absolute step rate : " << std::hex << absparams_rec.stepRate << endl;
-  cout << "move_absolute step accn : " << std::hex << absparams_rec.stepAccn << endl;
+  cout << "move_absolute step accn : " << std::hex << absparams_rec.stepAccn << endl;*/
   
   //motors.get_status_update(foxtrot::devices::destination::sourceTIM101);
   
-  foxtrot::devices::pos_counter_params posparams_rec = motors.request_position_counter(foxtrot::devices::destination::sourceTIM101);
-  
-  cout << "Get position counter says: " << std:: hex << posparams_rec.position << endl;
-  
-  
-  
-  */
+  //foxtrot::devices::pos_counter_params posparams_rec = motors.request_position_counter(foxtrot::devices::destination::sourceTIM101);
+  //cout << "Get position counter says: " << std:: hex << posparams_rec.position << endl;
+
   
   //motors.absolute_move(foxtrot::devices::destination::sourceTIM101,foxtrot::devices::motor_channel_idents::channel_1, 0x64);
   
