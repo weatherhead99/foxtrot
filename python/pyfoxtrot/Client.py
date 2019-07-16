4a6e89dc958eaa2e315a973327588ab867756678
Dan Weatherill <daniel.weatherill@physics.ox.ac.uk>
2019-07-16 17:00:35 +0100

    tidy up client and plumb in new logic

--- python/pyfoxtrot/Client.py
+++ python/pyfoxtrot/Client.py
@@ -5,173 +5,70 @@ Created on Wed Apr 19 11:44:13 2017
 
 @author: weatherill
 """
-from functools import wraps
-from pyfoxtrot.foxtrot_pb2_grpc import *
-from pyfoxtrot.foxtrot_pb2 import *
-from pyfoxtrot.ft_types_pb2 import *
-from pyfoxtrot.Errors import *
-from pyfoxtrot.common import _check_repl_err
-from pyfoxtrot.server_flags import ServerFlag, FlagProxy
 
-import types
 import struct
-
-import mypy
-
-DEFAULT_CHUNKSIZE=1000
-
-value_type_to_return_field_dict = {FLOAT_TYPE : ('dblret', float),
-                                   INT_TYPE   : ('intret',int),
-                                    BOOL_TYPE : ('boolret',bool),
-                                  STRING_TYPE : ('stringret',bytes)
-}
-
-def _capability_argument_from_value(val):
-    arg = capability_argument()
-    
-    if isinstance(val,float):
-        arg.dblarg = val
-    #not isinstance(True,int) is True, so using 
-    #isinstance() here false positives bool values
-    elif type(val) is int:
-        arg.intarg = val
-    elif isinstance(val,bool):
-        arg.boolarg = val
-    elif isinstance(val,str):
-        arg.strarg = val.encode("ASCII")
-    elif isinstance(val,bytes):
-        arg.strarg = val
-
-    return arg        
-
-def _fill_capability_args(args):
-    out = []
-    for idx,arg in enumerate(args):
-        a = _capability_argument_from_value(arg)
-        a.position = idx
-        out.append(a)
-    return out
+import grpc
+from .foxtrot_pb2_grpc import exptserveStub
+from .common import _check_repl_err
+from .server_flags import ServerFlag, FlagProxy
+from .TypeConversion import ft_variant_from_value, value_from_ft_variant
+from .foxtrot_pb2 import empty, capability_argument, broadcast_notification
+from .foxtrot_pb2 import chunk_request, capability_request, VALUE_READONLY
+from .foxtrot_pb2 import VALUE_READWRITE, ACTION, STREAM, UCHAR_TYPE, USHORT_TYPE
+from .foxtrot_pb2 import UINT_TYPE, BDOUBLE_TYPE
 
 
-def _construct_args(argnames, *args,**kwargs):
-    rawargs = [None] * len(argnames)
-    for name, val in kwargs.items():
-        if name not in argnames:
-            raise KeyError("passed an unrecognized keyword arg")
-        pos = argnames.index(name)
-        rawargs[pos] = val
-
-    
-    if len(args) + len(kwargs) > len(argnames):
-        raise ValueError("too many arguments provided")
-        
-    for idx,val in enumerate(args):
-        if rawargs[idx] is not None:
-            raise IndexError("conflicting positional and keyword arguments")
-        rawargs[idx] = val
-    
-    if any( _ is None for _ in rawargs ):
-        raise IndexError("not all arguments filled in")
-        
-    return _fill_capability_args(rawargs)
-    
-
-def _process_sync_response(repl,streamraw=False):
-    if hasattr(repl,"__iter__"):
-        rawbytes = bytearray()
-        
-        for chunk in repl:
-            _check_repl_err(chunk)
-            dtp = chunk.dtp
-            rawbytes += chunk.data
-                        
-        if streamraw:
-            return rawbytes
-            
-        if dtp == UCHAR_TYPE:
-            print("UCHAR")
-            return list(rawbytes)
-        elif dtp == USHORT_TYPE:
-            structstr = '<%dH' % (len(rawbytes) // 2)
-        
-        elif dtp == UINT_TYPE:
-            structstr = '<%dI' % (len(rawbytes) // 4)
-            
-        elif dtp == BDOUBLE_TYPE:
-            print("BDOUBLE")
-            structstr = '<%dd' % (len(rawbytes) // 8)
-        
-        else:
-            raise NotImplementedError("data type not supported yet")
-        
-        return list(struct.unpack(structstr,rawbytes))
-        
-        
-    else:
-        _check_repl_err(repl)
-        whichattr = repl.WhichOneof("return")
-        if whichattr is None:
-            return
-        ret = getattr(repl,whichattr)
-        if whichattr == "stringret" and len(ret) == 0:
-            return None
-        return ret
-        
-        
-        
-def _reinterpret_cast_bytes(bts,tp):
-    pass
-
+DEFAULT_CHUNKSIZE = 1000
 
 class Client:
-    def __init__(self,connstr: str, certfile:str=None) -> None:
+    def __init__(self, connstr: str, certfile: str = None) -> None:
         if certfile is None:
             self._channel = grpc.insecure_channel(connstr)
         else:
-            with open(certfile,"rb") as f:
+            with open(certfile, "rb") as f:
                 cert = f.read()
             creds = grpc.ssl_channel_credentials(root_certificates=cert)
-            self._channel = grpc.secure_channel(connstr,creds)
+            self._channel = grpc.secure_channel(connstr, creds)
         self._stub = exptserveStub(self._channel)
-        
+
         self._servdescribe = self._stub.DescribeServer(empty())
         self._comment = self._servdescribe.servcomment
-        
+
         self._setup_device_tree()
-        
-    def _setup_device_tree(self) -> None: 
+
+    def _setup_device_tree(self) -> None:
         self._devices = []
-        
+
         for devkey in self._servdescribe.devs_attached.keys():
             dev = self._servdescribe.devs_attached.get(devkey)
-            self._devices.append(Device(dev.devid, dev.devtype, dev.caps, dev.devcomment,self))            
-        
-        
+            self._devices.append(Device(dev.devid, dev.devtype, dev.caps,
+                                        dev.devcomment, self))
+
+
         for dev in self._devices:
-            if len(dev._comment) > 0:
-                setattr(self,dev._comment,dev)
+            if dev._comment:
+                setattr(self, dev._comment, dev)
             else:
-                setattr(self,dev._devtp,dev)
-    
-    def save_servdesc(self,fname):
+                setattr(self, dev._devtp, dev)
+
+    def save_servdesc(self, fname):
         ss = self._servdescribe.SerializeToString()
-        with open(fname,'wb') as f:
+        with open(fname, 'wb') as f:
             f.write(ss)
 
-    def __getitem__(self,keystr):
-        if not hasattr(self,"_devtypes"):
+    def __getitem__(self, keystr):
+        if not hasattr(self, "_devtypes"):
             self._devtypes = [_._devtp for _ in self._devices]
             self._devcomments = [_._comment for _ in self._devices]
             self._devids = [_._devid for _ in self._devices]
 
-        if isinstance(keystr,int):
+        if isinstance(keystr, int):
             return self._devices[self._devids.index(keystr)]
 
-        else:
-            if keystr in self._devcomments:
-                return self._devices[self._devcomments.index(keystr)]
+        if keystr in self._devcomments:
+            return self._devices[self._devcomments.index(keystr)]
 
-            return self._devices[self._devtypes.index(keystr)]    
+        return self._devices[self._devtypes.index(keystr)]
 
     def get_all_flags_dict(self) -> dict:
         return {_.name : _ for _ in self.flags}
@@ -181,14 +78,14 @@ class Client:
 
 
     def create_new_flag(self, name: str, initval):
-        sf = ServerFlag(self,name)
+        sf = ServerFlag(self, name)
         sf.value = initval
         return sf
 
     def drop_server_flag(self, flag) -> None:
-        if isinstance(flag,str):
-            ServerFlag(self,flag).drop()
-        elif isinstance(flag,ServerFlag):
+        if isinstance(flag, str):
+            ServerFlag(self, flag).drop()
+        elif isinstance(flag, ServerFlag):
             flag.drop()
 
     def broadcast_notification(self, body: str, title: str = None, channel: str = None):
@@ -198,81 +95,47 @@ class Client:
             req.title = ""
         else:
             req.title = title
-        
+
         if channel is None:
             req.use_default_channel = True
             req.channel_target = ""
         else:
             req.channel_target = channel
-        
+
         repl = self._stub.BroadcastNotification(req)
         _check_repl_err(repl)
-    
+
     @property
     def flags(self):
         return FlagProxy(self)
 
 
-def _fake_call_sync(obj,client,*args,**kwargs):
-    req = obj.construct_request(*args,**kwargs)
-    
-    if len(args) + len(kwargs) != len(req.args):
-        raise IndexError("incorrect number of arguments to dummy func")
-    
-    repl = datachunk() if obj._captp == STREAM else capability_response()
-    
-    repl.msgid = req.msgid
-    repl.capname = req.capname
-    repl.devid = req.devid
-    
-    if obj._captp == STREAM:
-        pass    
-    else:
-        if obj._rettp in value_type_to_return_field_dict:
-            attrname,tp = value_type_to_return_field_dict[obj._rettp]
-            setattr(repl,attrname,tp())
-        
-    return repl
-    
-
-class DummyClient(Client):
-    def __init__(self,sdfname):
-        with open(sdfname,'rb') as f:
-            ss = f.read()
-        self._servdescribe = servdescribe()
-        self._servdescribe.ParseFromString(ss)
-        self._setup_device_tree()
-        
-        for dev in self._devices:
-            for cap in dev._caps:
-                cap.call_cap_sync = types.MethodType(_fake_call_sync,cap)
-     
 
 class Device:
-    def __init__(self,devid,devtp, caps,comment,client):
+    def __init__(self, devid, devtp, caps, comment, client):
         self._devid = devid
         self._devtp = devtp
         self._comment = comment
         self._cl = client
-        
+
         self._caps = []
         for cap in caps:
-            self._caps.append(Capability(cap.tp,cap.capname,cap.argnames,cap.argtypes,cap.rettp,devid,client))
-        
+            self._caps.append(Capability(cap.tp, cap.capname, cap.argnames,
+                                         cap.argtypes, cap.rettp, devid, client))
+
         for cap in self._caps:
-            setattr(self,cap._capname,cap)
-        
+            setattr(self, cap._capname, cap)
+
     def __repr__(self):
-        if len(self._comment )> 0:
+        if self._comment:
             return str("Device(" + self._devtp + ": " + self._comment + ")")
-        
-        else:
-            return str("Device(" + self._devtp + ")")
+
+        return str("Device(" + self._devtp + ")")
 
 
 
 class Capability:
-    def __init__(self,captp,capname,argnames,argtypes,rettp,devid,client):
+    def __init__(self, captp, capname, argnames, argtypes, rettp, devid, client):
         self._captp = captp
         self._capname = capname
         self._argnames = argnames
@@ -284,7 +147,7 @@ class Capability:
         self._cl = client
 
     def __repr__(self):
-        if self._captp == VALUE_READONLY :
+        if self._captp == VALUE_READONLY:
             infostr = " (readonly value)"
         elif self._captp == VALUE_READWRITE:
             infostr = " (read/write value)"
@@ -292,39 +155,89 @@ class Capability:
             infostr = " (action)"
         else:
             infostr = " (data stream)"
-        
+
         if len(self._argnames) > 0:
             return self._capname + "(" + " ".join(self._argnames)  + ")"  + infostr
-        
-        else:
-            return self._capname + infostr
-        
-    def construct_request(self,*args,**kwargs):
+
+        return self._capname + infostr
+
+    def _construct_args(self, *args, **kwargs):
+        rawargs = [None] * len(self._argnames)
+        for name, val in kwargs.items():
+            if name not in self._argnames:
+                raise ValueError("no such argument with name: %s in capability"
+                                 % name)
+            pos = self._argnames.index(name)
+            argdesc = self._argtypes[pos]
+            rawargs[pos] = capability_argument(
+                pos=pos,
+                value=ft_variant_from_value(val, argdesc))
+
+        if len(args) + len(kwargs) > len(rawargs):
+            raise ValueError("too many arguments provided")
+
+        for idx, val in enumerate(args):
+            if rawargs[idx] is not None:
+                raise IndexError("conflicting positional and keyword arguments")
+            desc = self._argtypes[idx]
+            rawargs[idx] = capability_argument(pos=idx,
+                                               value=ft_variant_from_value(val, desc))
+
+        if any(_ is None for _ in rawargs):
+            raise IndexError("not all arguments filled in")
+
+        return rawargs
+
+    def _process_sync_response(self, repl):
+        if self._captp == STREAM:
+            rawbytes = bytearray()
+
+            for chunk in repl:
+                _check_repl_err(chunk)
+                dtp = chunk.dtp
+                rawbytes += chunk.data
+
+            if dtp == UCHAR_TYPE:
+                return list(rawbytes)
+            if dtp == USHORT_TYPE:
+                structstr = '<%dH' % (len(rawbytes) // 2)
+            elif dtp == UINT_TYPE:
+                structstr = '<%dI' % (len(rawbytes) // 4)
+            elif dtp == BDOUBLE_TYPE:
+                structstr = '<%dd' % (len(rawbytes) // 8)
+            else:
+                raise NotImplementedError("data type not supported yet")
+            return list(struct.unpack(structstr, rawbytes))
+
+        _check_repl_err(repl)
+        out = value_from_ft_variant(repl.returnval)
+        return out
+
+    def construct_request(self, *args, **kwargs):
         if self._captp != VALUE_READWRITE:
-            capargs = _construct_args(self._argnames,*args,**kwargs)
+            capargs = self._construct_args(self._argnames, *args, **kwargs)
         elif len(args) > 0 or len(kwargs) > 0:
-            capargs = _construct_args(self._argnames,*args,**kwargs)
+            capargs = self._construct_args(self._argnames, *args, **kwargs)
         else:
-            capargs = _construct_args([],*args,**kwargs)
-            
-        
+            capargs = self._construct_args([], *args, **kwargs)
+
         reqtp = chunk_request if self._captp == STREAM else capability_request
-        
-        req = reqtp(msgid = self._msgid, devid = self._devid, capname = self._capname, args = capargs)        
-        self._msgid +=1
-        
+
+        req = reqtp(msgid=self._msgid, devid=self._devid,
+                    capname=self._capname, args=capargs)
+        self._msgid += 1
+
         if self._captp == STREAM:
             req.chunksize = self.chunksize
-            
+
         return req
-    
-    def call_cap_sync(self,client,*args,**kwargs):
+
+    def call_cap_sync(self, client, *args, **kwargs):
         stubfun = client._stub.FetchData if self._captp == STREAM else client._stub.InvokeCapability
-        ret = stubfun(self.construct_request(*args,**kwargs))
-        
+        ret = stubfun(self.construct_request(*args, **kwargs))
+
         return ret
-        
-    def __call__(self,*args,**kwargs):
-        repl = self.call_cap_sync(self._cl,*args,**kwargs)
-        return _process_sync_response(repl)
-    
+
+    def __call__(self, *args, **kwargs):
+        repl = self.call_cap_sync(self._cl, *args, **kwargs)
+        return self._process_sync_response(repl)
