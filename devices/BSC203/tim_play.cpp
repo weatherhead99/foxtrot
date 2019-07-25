c7a7fe4f96797c93a7377a8cbe3f964c1857b689
Sergio Garcia <sergio@pplxdaq04.nat.physics.ox.ac.uk>
2019-07-25 11:59:26 +0100

    Organising tim_play better

--- devices/BSC203/tim_play.cpp
+++ devices/BSC203/tim_play.cpp
@@ -16,9 +16,9 @@ foxtrot::parameterset sport_params{
 
 int main(int argc,char** argv)
 {
-    
+  
   foxtrot::setLogFilterLevel(sl::trace);
-    
+  
   cout << "TIM101 test..." << endl;
   
   auto sport = std::make_shared<foxtrot::protocols::SerialPort>(&sport_params);
@@ -37,32 +37,10 @@ int main(int argc,char** argv)
   jogstruct.jogStepRate = 0x000001F4;
   jogstruct.jogStepAccn = 0x000186A0;
   
-  motors.set_jog_parameters(foxtrot::devices::destination::sourceTIM101, jogstruct);
-  
-  foxtrot::devices::jogparams jogstruct_req = motors.request_jog_parameters(foxtrot::devices::destination::sourceTIM101);
-  
-  cout << "jog Mode requested: " << std::hex << jogstruct_req.jogMode << endl;
-  cout << "jog Mode Size requested: " << std::hex << jogstruct_req.jogStepSize << endl;
-  cout << "jog Step Accn requested: " << std::hex << jogstruct_req.jogStepAccn << endl;*/
+  motors.set_jog_parameters(foxtrot::devices::destination::sourceTIM101, jogstruct);*/
   
   //motors.jog_move(foxtrot::devices::destination::sourceTIM101,foxtrot::devices::motor_channel_idents::channel_1, foxtrot::devices::jogdir::forward);
 
-  // SET POSITION COUNTER
-  /*foxtrot::devices::pos_counter_params posparams;
-  posparams.subMsgID = 0x0005;
-  posparams.chanIndent = 0x01;
-  posparams.position = 0x0;
-  posparams.encCount = 0x0;
-  
-  motors.set_position_counter(foxtrot::devices::destination::sourceTIM101, posparams);
-  
-  motors.jog_move(foxtrot::devices::destination::sourceTIM101,foxtrot::devices::motor_channel_idents::channel_1, foxtrot::devices::jogdir::forward);
-  
-  motors.jog_move(foxtrot::devices::destination::sourceTIM101,foxtrot::devices::motor_channel_idents::channel_4, foxtrot::devices::jogdir::forward);*/
-  
-  //foxtrot::devices::pos_counter_params posparams_rec = motors.request_position_counter(foxtrot::devices::destination::sourceTIM101);
-  //cout << "Get position counter says: " << std:: hex << posparams_rec.position << endl;
-  
   
   //ABSOLUTE MOVE
   /*foxtrot::devices::move_absolute_params absparams;
@@ -74,25 +52,7 @@ int main(int argc,char** argv)
   
   motors.set_move_absolute_parameters(foxtrot::devices::destination::sourceTIM101, absparams);*/
   
-  foxtrot::devices::move_absolute_params absparams_rec = motors.request_move_absolute_parameters(foxtrot::devices::destination::sourceTIM101);
+  //motors.absolute_move(foxtrot::devices::destination::sourceTIM101,foxtrot::devices::motor_channel_idents::channel_1, 0x14);
   
-  cout << "move_absolute channel : " << std::hex << absparams_rec.chanIndent << endl;
-  cout << "move_absolute max voltage : " << std::hex << absparams_rec.maxVoltage << endl;
-  cout << "move_absolute step rate : " << std::hex << absparams_rec.stepRate << endl;
-  cout << "move_absolute step accn : " << std::hex << absparams_rec.stepAccn << endl;
-  
-  //motors.get_status_update(foxtrot::devices::destination::sourceTIM101);
-  
-  foxtrot::devices::pos_counter_params posparams_rec = motors.request_position_counter(foxtrot::devices::destination::sourceTIM101);
-  
-  cout << "Get position counter says: " << std:: hex << posparams_rec.position << endl;
-  
-  
-  
-  */
-  
-  //motors.absolute_move(foxtrot::devices::destination::sourceTIM101,foxtrot::devices::motor_channel_idents::channel_1, 0x64);
-  
-  //foxtrot::devices::motor_status motorstr = motors.get_status_update(foxtrot::devices::destination::sourceTIM101);
 
 };
--- lsst/motor_test_setup.cpp
+++ lsst/motor_test_setup.cpp
@@ -1,17 +1,72 @@
 #include <foxtrot/server/DeviceHarness.h>
+#include <foxtrot/protocols/SerialPort.h>
+#include <map>
+/*
+#include <foxtrot/devices/BSC203.h>
+#include <foxtrot/devices/TIM101.h>*/
+
+
+using mapofparametersets = std::map<std::string, foxtrot::parameterset>;
+/*
+foxtrot::parameterset sport_params_tim {
+    {"port", "/dev/ttyUSB0"},
+    {"flowcontrol", "hardware"}
+};
+
+foxtrot::parameterset sport_params_bsc {
+    {"port", "/dev/ttyUSB1"},
+    {"flowcontrol", "hardware"}
+};
+
+template<typename T> void setup_with_disable(const std::string& device_name, std::map<std::string,parameter>& params,
+					     foxtrot::Logging& lg, T fun)
+{
+  
+  auto disable_str = "disable_" + device_name;
+  if(!boost::get<int>(params[disable_str]))
+  {
+    fun();
+  }
+  else
+  {
+    lg.strm(sl::warning) << "device: " << device_name << " was disabled in the config file...";
+  }
+  
+};*/
+
 
 extern "C" {
 
-int setup(foxtrot::DeviceHarness& harness,
-          const mapofparametersets* const params)
-{
-    
-    
-    return 0;
-    
-}
-    
-    
+    int setup(foxtrot::DeviceHarness& harness, const mapofparametersets* const params)
+    {
+        /*foxtrot::Logging lg("setup");
 
+        if(params == nullptr)
+        {
+            lg.Fatal("no parametersets received, cannot continue..");
+            throw std::runtime_error("setup cannot continue");
+        }
 
+        auto setup_params = params->at("setup");
+        auto tpg_params = params->at("tpg_params");
+        
+        setup_with_disable("TIM101", setup_params, lg,[&harness, &tpg_params, &lg] () {
+            lg.Info("setting up TIM101");
+            auto sport_tim = std::make_shared<foxtrot::protocols::SerialPort>(&sport_params_tim);
+
+            auto presgauge_tim = std::unique_ptr<foxtrot::devices::TIM101> (new foxtrot::devices::TIM101(sport_tim));
+            harness.AddDevice(std::move(presgauge_tim));
+        });
+        
+        setup_with_disable("BSC203", setup_params, lg,[&harness, &tpg_params, &lg] () {
+            lg.Info("setting up BSC203");
+            auto sport_bsc = std::make_shared<foxtrot::protocols::SerialPort>(&sport_params_bsc);
+
+            auto presgauge_bsc = std::unique_ptr<foxtrot::devices::BSC203> (new foxtrot::devices::TIM101(sport_bsc));
+            harness.AddDevice(std::move(presgauge_bsc));
+        });*/
+        
+        return 0;
+
+    }
 }
