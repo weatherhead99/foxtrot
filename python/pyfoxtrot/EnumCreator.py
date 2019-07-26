44ec48183d9a5e224636abeed05828a26c4da075
Dan Weatherill <daniel.weatherill@physics.ox.ac.uk>
2019-07-26 14:06:44 +0100

    add missing EnumCreator.py

---
+++ python/pyfoxtrot/EnumCreator.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 18 12:54:38 2019
+
+@author: weatherill
+"""
+
+from enum import IntEnum
+from pyfoxtrot.ft_types_pb2 import enum_descriptor
+
+def define_enum(desc: enum_descriptor):
+    newtypename = desc.enum_name.replace("::","_")
+    tp = IntEnum(newtypename, dict(desc.enum_map.items()))
+
+    return tp
