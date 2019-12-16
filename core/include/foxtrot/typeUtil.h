e637c1ef91fe73f7c1a0f736706b175819f02573
Daniel Weatherill <weatherill@physics.ox.ac.uk>
2019-12-16 18:12:15 +0000

    fix a bunch of warnings & errors in MSVC

--- core/include/foxtrot/typeUtil.h
+++ core/include/foxtrot/typeUtil.h
@@ -3,37 +3,38 @@
 #include <rttr/type>
 #include <foxtrot/ft_types.pb.h>
 #include <utility>
+#include <foxtrot/foxtrot_core_export.h>
 
 namespace foxtrot{
     class Logging;
     
-    bool is_POD_struct(const rttr::type& tp, Logging* lg=nullptr);
-    bool is_tuple(const rttr::type& tp, Logging* lg = nullptr);
+    FOXTROT_CORE_EXPORT bool is_POD_struct(const rttr::type& tp, Logging* lg=nullptr);
+    FOXTROT_CORE_EXPORT bool is_tuple(const rttr::type& tp, Logging* lg = nullptr);
     
-    ft_simplevariant get_simple_variant_wire_type(const rttr::variant& var, Logging* lg=nullptr);
+    FOXTROT_CORE_EXPORT ft_simplevariant get_simple_variant_wire_type(const rttr::variant& var, Logging* lg=nullptr);
     
-    ft_struct get_struct_wire_type(const rttr::variant& var, Logging* lg=nullptr);
-    ft_enum get_enum_wire_type(const rttr::variant& var, Logging* lg=nullptr);
-    ft_tuple get_tuple_wire_type(const rttr::variant& var, Logging* lg = nullptr);
+    FOXTROT_CORE_EXPORT ft_struct get_struct_wire_type(const rttr::variant& var, Logging* lg=nullptr);
+    FOXTROT_CORE_EXPORT ft_enum get_enum_wire_type(const rttr::variant& var, Logging* lg=nullptr);
+    FOXTROT_CORE_EXPORT ft_tuple get_tuple_wire_type(const rttr::variant& var, Logging* lg = nullptr);
     
-    ft_variant get_variant_wire_type(const rttr::variant& var, Logging* lg=nullptr);
+    FOXTROT_CORE_EXPORT ft_variant get_variant_wire_type(const rttr::variant& var, Logging* lg=nullptr);
     
     
-    rttr::variant wire_type_to_variant(const ft_variant& wiretp, 
+    FOXTROT_CORE_EXPORT rttr::variant wire_type_to_variant(const ft_variant& wiretp, 
                                        const rttr::type& target_tp, Logging* lg=nullptr);
     
-    rttr::variant wire_type_to_variant(const ft_simplevariant& wiretp, 
+    FOXTROT_CORE_EXPORT rttr::variant wire_type_to_variant(const ft_simplevariant& wiretp, 
                                        const rttr::type& target_tp, Logging* lg=nullptr);
-    rttr::variant wire_type_to_variant(const ft_struct& wiretp, 
+    FOXTROT_CORE_EXPORT rttr::variant wire_type_to_variant(const ft_struct& wiretp, 
                                        const rttr::type& target_tp, Logging* lg=nullptr);
-    rttr::variant wire_type_to_variant(const ft_enum& wiretp,
+    FOXTROT_CORE_EXPORT rttr::variant wire_type_to_variant(const ft_enum& wiretp,
                                        const rttr::type& target_tp, Logging* lg=nullptr);
     
-    variant_descriptor describe_type(const rttr::type& tp, Logging* lg=nullptr);
+    FOXTROT_CORE_EXPORT variant_descriptor describe_type(const rttr::type& tp, Logging* lg=nullptr);
     
-    std::pair<simplevalue_types, unsigned char> describe_simple_type(const rttr::type& tp, Logging* lg=nullptr);
-    struct_descriptor describe_struct(const rttr::type& tp, Logging* lg=nullptr);
-    enum_descriptor describe_enum(const rttr::type& tp, Logging* lg=nullptr);
-    tuple_descriptor describe_tuple(const rttr::type& tp, Logging* lg=nullptr);
+    FOXTROT_CORE_EXPORT std::pair<simplevalue_types, unsigned char> describe_simple_type(const rttr::type& tp, Logging* lg=nullptr);
+    FOXTROT_CORE_EXPORT struct_descriptor describe_struct(const rttr::type& tp, Logging* lg=nullptr);
+    FOXTROT_CORE_EXPORT enum_descriptor describe_enum(const rttr::type& tp, Logging* lg=nullptr);
+    FOXTROT_CORE_EXPORT tuple_descriptor describe_tuple(const rttr::type& tp, Logging* lg=nullptr);
     
 }
