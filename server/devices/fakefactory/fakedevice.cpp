9d4e672fe4f1d69fdd2b716e786747f93dd05a76
Dan Weatherill <daniel.weatherill@physics.ox.ac.uk>
2023-05-15 16:10:16 +0100

    minor fake device tweaks

--- server/devices/fakefactory/fakedevice.cpp
+++ server/devices/fakefactory/fakedevice.cpp
@@ -6,20 +6,27 @@ using namespace foxtrot;
 using namespace foxtrot::devices;
 using std::string;
 
-fakeDeviceFactory::fakeDeviceFactory(shared_ptr<DeviceHarness> harness,
-                                              int n_devices_avail,
-                                              const string &comment)
-  : DeviceFactory(harness, comment), _n_devices_avail(n_devices_avail), _impl(new fakeDeviceImpl) {}
-
-
-fakeDeviceFactory::~fakeDeviceFactory
-
-
-
-class fakeDeviceImpl
+class foxtrot::devices::fakeDeviceFactoryImpl
 {
   string _value;
   std::default_random_engine eng;
   
 
 };
+
+
+
+
+fakeDeviceFactory::fakeDeviceFactory(shared_ptr<DeviceHarness> harness,
+                                              int n_devices_avail,
+                                              const string &comment)
+  : DeviceFactory(harness, comment), _n_devices_avail(n_devices_avail), _impl(new fakeDeviceFactoryImpl) {}
+
+
+fakeDeviceFactory::~fakeDeviceFactory()
+{
+
+}
+
+
+
