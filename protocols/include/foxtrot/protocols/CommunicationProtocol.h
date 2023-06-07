ada6e2751768e9c3252c59a51542584b69a4b432
Dan Weatherill <daniel.weatherill@physics.ox.ac.uk>
2023-06-07 16:37:44 +0100

    add unsigned to parameter variant

--- protocols/include/foxtrot/protocols/CommunicationProtocol.h
+++ protocols/include/foxtrot/protocols/CommunicationProtocol.h
@@ -12,7 +12,7 @@ namespace foxtrot
   
   using parameter_types = boost::mpl::vector<unsigned,char, int , std::string>;
   using parameter_types_minimal = boost::mpl::vector<int,std::string>;
-  using parameter = std::variant<int, std::string>;
+  using parameter = std::variant<int, unsigned, std::string>;
   using parameterset = std::map<std::string, parameter>;
   
   
