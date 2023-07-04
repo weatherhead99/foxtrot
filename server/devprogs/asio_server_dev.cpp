1413ba0f3bd85b968ddfb79822c89a11a302561c
Dan Weatherill <daniel.weatherill@physics.ox.ac.uk>
2023-07-04 17:13:14 +0100

    fathoming the mysteries of asio-grpc, slowly...

--- server/devprogs/asio_server_dev.cpp
+++ server/devprogs/asio_server_dev.cpp
@@ -5,22 +5,43 @@
 #include <grpcpp/server.h>
 #include <grpcpp/server_builder.h>
 #include <boost/asio/thread_pool.hpp>
+#include <boost/asio/spawn.hpp>
 
 #include <foxtrot/Logging.h>
 #include <foxtrot/foxtrot.grpc.pb.h>
+#include <agrpc/asio_grpc.hpp>
 
 
 namespace asio = boost::asio;
-//using agrpc::GrpcContext;
-
-
-
-
-
+using agrpc::GrpcContext;
 
 
 using std::cout;
 using std::endl;
+
+
+
+foxtrot::exptserve::AsyncService service;
+
+template<typename H>
+void handler(asio::basic_yield_context<H>& yield)
+{
+  grpc::ServerContext sctxt;
+  foxtrot::empty req;
+
+  grpc::ServerAsyncResponseWriter<foxtrot::server_info> writer{&sctxt};
+
+  agrpc::request(&foxtrot::exptserve::AsyncService::RequestGetServerInfo,
+		 service, sctxt, req, writer, yield);
+  
+
+  foxtrot::server_info repl;
+  repl.set_rpc_version(12);
+
+
+}
+
+
 
 
 
@@ -29,22 +50,21 @@ int main()
   cout << "hello" << endl;
   foxtrot::setDefaultSink();
   
-  
-
+ 
   grpc::ServerBuilder builder;
 
-  //  GrpcContext ctxt(builder.AddCompletionQueue());
+  GrpcContext ctxt(builder.AddCompletionQueue());
+
+  asio::basic_yield_context<agrpc::GrpcExecutor> yield;
+  
+  auto exec = ctxt.get_executor();
 
   builder.AddListeningPort("localhost:50051", grpc::InsecureServerCredentials());
 
-  foxtrot::exptserve::AsyncService service;
   builder.RegisterService(&service);
   auto serv = builder.BuildAndStart();
 
-
-  asio::thread_pool thread_pool(1);
-  
-  
+  //  asio::spawn(ctxt, handler<decltype(exec)>);;
   
 
 }
