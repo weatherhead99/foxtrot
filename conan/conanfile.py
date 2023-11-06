aad2db9e7d8f1becf7bb41260bca2f4482ad2af5
Dan Weatherill <daniel.weatherill@physics.ox.ac.uk>
2023-11-06 17:08:43 +0000

    fix bad python formatting

--- conan/conanfile.py
+++ conan/conanfile.py
@@ -90,8 +90,8 @@ class FoxtrotCppPackage:
             self.output.error(f"current cpp standard setting is: {self.settings.compiler.cppstd}")
             self.output.error("failed check requiring minimum of c++17")
             raise ConanInvalidConfiguration("foxtrot modules require at least c++17 standard to build")
- 
-   def set_version(self):
+
+    def set_version(self):
         if hasattr(self, "conan_data") and "frozen_version" in self.conan_data:
             self.version = self.conan_data["frozen_version"]
         else:
