5f96493f43c9c5ff0f74afad63c0e9b418eb2a99
Dan Weatherill <daniel.weatherill@physics.ox.ac.uk>
2024-08-13 15:06:33 +0100

    update hatch build script

--- python/hatch_build.py
+++ python/hatch_build.py
@@ -1,16 +1,27 @@
 from hatchling.builders.hooks.plugin.interface import BuildHookInterface
 
+from typing import Any
 from grpc_tools import command
-from shutil import copyfile
+from shutil import copyfile, rmtree
 import os
 
 class ProtobufBuildHook(BuildHookInterface):
     PLUGIN_NAME="ft_protobuf"
-    
-    def clean(self):
-        pass
 
-    def initialize(self, version, build_data):
+    @property
+    def _proto_output_dir(self) -> str:
+        if "proto_output_dir" not in self.config:
+            raise ValueError("need to specify proto_output_dir in config")
+        return self.config["proto_output_dir"]
+
+
+    def clean(self, versions: list[str]) -> None:
+        if os.path.exists(self._proto_output_dir):
+            self.app.display_info("cleaning proto output dir")
+            rmtree(self._proto_output_dir)
+
+
+    def initialize(self, version: str, build_data: dict[str, Any]) -> None:
         self.app.display_info("in protobuf build hook!")
 
         print(f"directory: {self.directory}")
@@ -21,15 +32,17 @@ class ProtobufBuildHook(BuildHookInterface):
         
         if "proto_source_dir" not in self.config:
             raise ValueError("need to specify proto_source_dir")
-        if "proto_output_dir" not in self.config:
-            raise ValueError("need to specify proto_output_dir")
 
         sdir = self.config["proto_source_dir"]
-        ddir = self.config["proto_output_dir"]
+        ddir = self._proto_output_dir
         proto_fls = [_ for _ in os.listdir(sdir) if ".proto" in _]
         self.app.display_info(f"protocol definition files: {proto_fls}")
         self.app.display_info(f"copying proto definitions...")
 
+        if not os.path.exists(ddir):
+            self.app.display_info("target directory doesn't exist, making it")
+            os.mkdir(ddir)
+        
         for fl in proto_fls:
             copyfile(os.path.join(sdir, fl), os.path.join(ddir, fl))
 
