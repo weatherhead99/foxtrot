06e68c19a57de02be1f375b4654a1f21dca45923
Dan Weatherill <daniel.weatherill@physics.ox.ac.uk>
2024-10-17 19:03:45 +0100

    try again to fix exports_sources

--- devices/conanfile.py
+++ devices/conanfile.py
@@ -10,16 +10,9 @@ class FoxtrotDevicesConan(ConanFile):
     name="foxtrot_devices"
     src_folder="devices"
     description="device drivers for foxtrot"
-    exports_sources= ("CMakeLists.txt", "cmake/*.in", "archon/*.cpp",
-                      "archon/*.h", "BME280/*.cpp", "BME280/*.h",
-                      "Pfeiffer/*.h", "Pfeiffer/*.hh", "Thorlabs_APT/*.cpp", "cornerstone_260/*.h",
-                      "cornerstone_260/*.cpp", "DM3068/*.cpp", "DM3068/*.h",
-                      "newport_2936R/*.h", "newport_2936R/*.cpp", "OPS-Q250/*.h",
-                      "OPS-Q250/*.cpp", "stellarnet/*.h", "stellarnet/*.cpp",
-                      "TPG362/*.h", "TPG362/*.cpp", "webswitch_plus/*.h",
-                      "DLP-IOR4/*.cpp", "DLP-IOR4/*.hh", "RLY-8/*.hh", "RLY-8/*.cpp",
-                      "webswitch_plus/*.cpp", "*/CMakeLists.txt", "cmake/Find*.cmake",
-                      "idscamera/*.cpp", "idscamera/*.h")
+
+    exports_sources = ("*/CMakeLists.txt", "cmake/*.in",
+                       "*/*.cpp", "*/*.h", "*/*.hh", "cmake/Find*.cmake")
 
     requires =  ("libusb/[^1.0.26]")
 
